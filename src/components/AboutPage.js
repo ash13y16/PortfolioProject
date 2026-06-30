91919d5b86463c1f858b26530704de7483144d22
ash13y16 <ashley20854@gmail.com>
2026-06-30 12:21:24 -0400

    Saving progress on about

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -60,43 +60,52 @@ function AboutPage() {
             <div className="half">
                 <h1>About</h1>
                 <p className={"intro"}>Look below for a little bit about me. </p>
-                <p className={"paragraph"}>I am an undergraduate at the University of Wisconsin - Madison
-                    pursuing a bachelor’s degree in Computer Science and Data Science, and a certificate in Business.
-                    When I am not on campus, I live in the DC area. I chose to study computer and data science
-                    because I am able to challenge myself in a way that I have not found in other areas of study.
-                    I love seeking out problems and coming up with innovative solutions, which is a skill that
-                    computer science requires of its students. Coding is often complicated and frustrating, and
-                    bugs are inevitable. This is why having a love of problem solving comes in handy when studying
-                    computer science. I do not get discouraged when I face setbacks, but rather look at them as
-                    opportunities to challenge myself to find a more creative solution. For example, designing my
-                    portfolio website had been an interesting challenge for me since I had never designed a website
-                    from scratch before. I had been working on the outline using a graphic design tool,
-                    but it wasn’t coming together the way I wanted it too. I took a break only to come back and
-                    find that my work hadn't saved. Instead of getting discouraged, I took it as an opportunity to
-                    start fresh and ended up being much happier with the new design then I had been with the old one.
-
-                    <br/>
-                    This seems like a good idea, or I could just keep typing
-                    until I am finished or can't come up with anything else to
-                    type!
-                    <br/>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Nulla viverra eros lorem, eget pulvinar erat scelerisque quis.
-                    In sodales quis tortor sed elementum. Fusce sollicitudin lacinia velit,
-                    at viverra justo. Aliquam tincidunt enim dolor, vel sodales risus placerat sed.
-                    Vivamus et tempor sapien, in tincidunt mi. In hac habitasse platea dictumst.
-                    Phasellus ultrices nisl id quam euismod, in posuere ante egestas.
-                    Donec imperdiet ante non luctus consequat. Phasellus tincidunt pharetra dolor
-                    sed pharetra. Etiam rutrum quam malesuada ligula laoreet, et
-                    porta felis viverra. Integer id dignissim arcu. Pellentesque quam augue,
-                    convallis at bibendum dignissim, congue a mi.
-                </p>
+                <div className={"paragraph"}>
+                    <p>I am an undergraduate at the University of Wisconsin - Madison pursuing a bachelor’s degree in
+                        Computer Science and Data Science, and a certificate in Business. I am a dedicated student who
+                        has been on the Dean’s Honors List for three semesters, and I have worked on classroom projects
+                        that covered various core CS topics such as data structures, object oriented programming,
+                        machine learning and website scraping. I have also done personal projects with React,
+                        JavaScript, Python and HTML, including two internships where I worked on building websites for
+                        companies to display their business and attract customers using a professional looking and easy
+                        to navigate user interface.</p>
+                    <p>At my internship at Williams & Williams Law Offices, I worked on both front and back end
+                        development by building and launching the website from scratch using Reach and JavaScript.
+                        Additionally, I published the website and managed the Google Profile for the business to attract
+                        customers.</p>
+                    <p>When I worked at Minnow Designs, I collaborated closely with management on the front end to
+                        ensure it was fully customizable and correctly displayed the brand’s image, message, and
+                        products to customers. Additionally, I thoroughly communicated with management to facilitate
+                        effective and efficient asynchronous work, resulting in a faster launch of the website.</p>
+                    <p>Throughout my time learning and working with these skills, I have been a passionate and dedicated
+                        student who actively seeks out answers to questions and new challenges.</p>
+                    <p>I chose to study computer and data science because I love seeking out problems and coming up with
+                        innovative solutions, which is a skill that computer science requires of its students. Coding is
+                        often complicated and frustrating, and bugs are inevitable. This is why having a love of problem
+                        solving comes in handy when studying computer science. I do not get discouraged when I face
+                        setbacks, but rather look at them as opportunities to challenge myself to find a more creative
+                        solution. For example, designing my portfolio website had been an interesting challenge for me
+                        since I had never designed a website from scratch before. I had been working on the outline
+                        using a graphic design tool, but it wasn’t coming together the way I wanted it too. I took a
+                        break only to come back and find that my work hadn't saved. Instead of getting discouraged, I
+                        took it as an opportunity to start fresh and ended up being much happier with the new design
+                        then I had been with the old one.</p>
+                    <p>I am a dedicated team member who goes above and beyond to ensure my team is working efficiently.
+                        I worked as a lifeguard at Headfirst's largest summer camp in the DC area. At this position, I
+                        completed my required tasks of enforcing safety protocols, ensuring the well-being of campers
+                        and helping complete day to day operations, and I went beyond these expectations by helping my
+                        team leader complete weekly swim reports. I communicated with swim instructors and team members
+                        to complete individual swim reports for over 150 campers each week, which highlighted the
+                        results of instructional swim with campers and kept parents informed about their child's
+                        swimming abilities.</p>
+                </div>
                 <hr/>
                 <a href={"mailto:ashley.w8140@gmail.com"}>
                     <button className={"contactMeEmail"} id={"openEmail"}>Email Me</button>
                 </a>
             </div>
-            <div className="half" id={"rightHalf"} style={{display: "flex", justifyContent: "flex-end" }}>
+            <div className="half" style={{
+                position: "sticky", top: "30px"}}>
                 <img src={iceImage} alt = "image"/>
             </div>
         </div>
