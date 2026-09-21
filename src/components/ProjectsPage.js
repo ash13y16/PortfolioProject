1964bf84f293e2d6a863a7fd53c977f1bc848bcf
ash13y16 <ashley20854@gmail.com>
2026-09-20 22:51:22 -0500

    Changing link color

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -8,8 +8,8 @@ function ProjectsPage() {
         <div>
             <h1 className={"projects"} id={"projects-title"}>Projects</h1>
             <div className={"projects"} id={"hover-instrs"}>
-                Hover over an image to see more details.<br/>
-                <a href={"https://github.com/ash13y16"}>Click here to view my github.</a>
+                Hover over an image to see more details.
+                <a href={"https://github.com/ash13y16"}> Click here to view my github.</a>
             </div>
 
             <div className={"projects"}>
